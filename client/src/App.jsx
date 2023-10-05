bb3def24903558dc57087423202d0dc3464f397d
terenceDev <terence.arseneault@hotmail.com>
2023-10-05 19:25:42 -0300

    added client and server folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,59 @@ function App() {
 }
 
 export default App
+=======
+import React, { useRef } from "react";
+import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Services from "./components/Services";
+// import About from "./components/About";
+// import Menu from "./components/Menu";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
+// import "./App.css";
+
+function App() {
+  const heroRef = useRef(null);
+  const servicesRef = useRef(null);
+  const aboutRef = useRef(null);
+  const menuRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const sectionStyle = { marginBottom: "8em" };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar
+          heroRef={heroRef}
+          servicesRef={servicesRef}
+          aboutRef={aboutRef}
+          menuRef={menuRef}
+          contactRef={contactRef}
+        />
+        {/* <div className="center-content">
+          <div ref={heroRef} style={sectionStyle}>
+            <Hero menuRef={menuRef} contactRef={contactRef} />
+          </div>
+          <div ref={servicesRef} style={sectionStyle}>
+            <Services />
+          </div>
+          <div ref={aboutRef} style={sectionStyle}>
+            <About />
+          </div>
+          <div ref={menuRef} style={sectionStyle}>
+            <Menu />
+          </div>
+          <div ref={contactRef} style={sectionStyle}>
+            <Contact />
+          </div>
+        </div>
+        <Footer /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+>>>>>>> 26e42d0 (restructured to add client and server folders along with installed concurrently package to root folder)
