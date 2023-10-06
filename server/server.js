892f5087030a5d8d319d2c06aa92ce6a3858d2db
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-06 11:53:05 -0300

    added User models including bcrypt hashing

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3001;
+const db = require('./config/db');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
