dc324a7431c1f0d5f6cacec4f06b5127d3783214
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-06 13:22:38 -0300

    added userShema resolver which is incomplete as I havent finished auth.js / graphQLError

--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -1,1 +1,28 @@
 const User = require('./models/User');
+
+const resolvers = {
+    Query: {
+        getUser: async (_, { name }) => {
+            try {
+                const user = await User.findOne({ name });
+                return user;
+            } catch (error) {
+              //throw new Error('Error'" I need to add GraphQLError Still in Utils/auth.js")
+            }
+        },
+    },
+
+    Mutation: {
+        createUser: async (_, { name, email, password }) => {
+            try {
+                const user = new User({ name, email, password });
+                await user.save();
+                return user;
+            } catch (error) {
+                //throw new Error('error'"I need to add graphQLError Still in utils/auth.js")
+            }
+        },
+    },
+};
+
+module.exports = resolvers;
