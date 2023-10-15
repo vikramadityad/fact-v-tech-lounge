ab13031e655865dbd8f51403114efd7d36ca36c7
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-14 21:22:07 -0300

    added Login / Signup component

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>{isLogin ? 'Login' : 'Sign Up'}</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
+      </form>
+      <p onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? 'Need an account? Sign Up' : 'Already have an account? Login'}
+      </p>
+    </div>
+  );
+};
+
+export default Auth;
