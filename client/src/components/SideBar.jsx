92f70887b837c97ebf2f89700c1aee2ba708f522
terenceDev <terence.arseneault@hotmail.com>
2023-10-16 12:58:05 -0300

    added remaining 2 missing images to cloudinary and updated seed file

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -24,7 +24,7 @@ const SideBar = ({
   //Fetch statement to post cart items to stripe database
   const checkout = async () => {
     console.log(cartItems);
-    await fetch("http://localhost:5173/checkout", {
+    await fetch("http://localhost:4000/checkout", {
       method: "POST",
       headers: {
         "Content-Type": "application/JSON",
@@ -33,6 +33,7 @@ const SideBar = ({
       body: JSON.stringify({ items: cartItems }),
     })
       .then((response) => {
+        console.log(response);
         return response.json();
       })
       .then((response) => {
