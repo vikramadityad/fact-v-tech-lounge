37c6575f77dea1f4e65b4eff2657a1f9259bbae7
Fih <124883284+Firene11@users.noreply.github.com>
2023-10-16 23:59:29 -0300

    moved the confirmation to top of page when subscribed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -68,6 +68,11 @@ function App() {
             cartItems={cartItems}
             setCartItems={setCartItems}
           />
+
+      <Routes>
+          <Route path="confirmation" element={<Confirmation />} />
+      </Routes>
+
           <div className="center-content">
             <div ref={heroRef} style={sectionStyle}>
               <Hero menuRef={menuRef} contactRef={contactRef} />
@@ -99,9 +104,7 @@ function App() {
               <VipImage />
             </div>
 
-            <Routes>
-          <Route path="confirmation" element={<Confirmation />} />
-      </Routes>
+
 
             <div ref={contactRef} style={sectionStyle}>
               <ContactUs />
